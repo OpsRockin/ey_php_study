a2ec2e6d625ce1d8b31abbed8d24fbdf21e0e7c0
sawanoboly <sawanoboriyu@higanworks.com>
2014-02-19 18:03:45 +0900

    update hook skip install if wp exist.

--- deploy/before_bundle.rb
+++ deploy/before_bundle.rb
@@ -1,1 +1,3 @@
-run "wp core download --locale=ja --path=#{shred_path}/system/wordpress"
+unless File.exists?("#{shred_path}/system/wordpress")
+  run "wp core download --locale=ja --path=#{shred_path}/system/wordpress"
+end
