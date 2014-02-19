06837481bc26ce54f4c57ed169f86da41765bb78
sawanoboly <sawanoboriyu@higanworks.com>
2014-02-19 17:47:40 +0900

    add hook after migrate

--- deploy/after_bundle.rb
+++ deploy/after_bundle.rb
@@ -1,1 +1,1 @@
-run "ln -s #{shared_path}/system/wordpress #{release_path}/wordpress"
+run "ln -s #{shared_path}/system/wordpress #{release_path}/public"
---
+++ deploy/after_migrate.rb
@@ -0,0 +1,1 @@
+run "ln -sf #{shared_path}/config/wp-config.php #{shared_path}/system/wordpress/wp-config.php"
